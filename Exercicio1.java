09652a0727c0425542a9c4a64a74559b8531f4bd
gustavofre <gustavolfre@gmail.com>
2020-05-15 19:16:20 -0300

    Exercício 1 para Treinar Java

---
+++ Exercicio1.java
@@ -0,0 +1,25 @@
+package Exerciciotreino;
+
+
+
+public class Exercicio1 {
+
+	public static void main (String args[]){
+		int soma=0;
+		
+		for(int i=0; i <= 500; i++) {
+			if(i% 3 == 0) {
+				soma= soma + i;
+			}
+			
+		
+		
+	}
+		
+				System.out.println("Soma dos divisíveis por 3: " + soma);
+		
+	}
+	
+	
+}
+
