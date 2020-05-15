7f278dbcbc96fa24d30e9656b66250dec48fc127
gustavofre <gustavolfre@gmail.com>
2020-05-15 19:56:55 -0300

    Exercício 2 para treinar Java

---
+++ Exercicio2.java
@@ -0,0 +1,21 @@
+package Exerciciotreino;
+
+public class Exercicio2 {
+
+	public static void main (String args[]){
+	
+		int numeroinicial = 1000;
+		int numerofinal = 1999;
+		
+		for (int i=numeroinicial; i<=numerofinal; i++) {
+			if (i%11==5) {
+				System.out.println(i);
+			}
+		
+			
+		}
+			
+		
+	}
+		
+}
